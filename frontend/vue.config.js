ef1159e41f8726970b2ad47eb550c85200a2477c
Carter Capocaccia <ccapocaccia@uaminc.com>
2019-06-12 10:26:00 -0500

    init commit

---
+++ frontend/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // proxy API requests to Valet during development
+    devServer: {
+        proxy: 'http://fullstack.test'
+    },
+    // output built static files to Laravel's public dir.
+    // note the "build" script in package.json needs to be modified as well.
+    outputDir: '../public',
+    // modify the location of the generated HTML file.
+    // make sure to do this only in production.
+    indexPath: process.env.NODE_ENV === 'production'
+        ? '../resources/views/index.blade.php'
+        : 'index.html'
+}
